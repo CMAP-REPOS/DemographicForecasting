201aa017174025244d7d0dccc58e2eb0249b8bfa
maryweber <67429420+maryweber@users.noreply.github.com>
2021-01-28 10:30:46 -0600

    minor changes

--- src/Population.R
+++ src/Population.R
@@ -72,7 +72,7 @@ for (YEAR in YEARS) {
 
 # Read 1990 and 1995 data from spreadsheets
 POP[["1990"]] <- read_excel("Input/Pop1990.xlsx")  # Should be adjusted to match 2000/2010 format
-POP[["1995"]] <- read_excel("Input/Pop1995.xlsx")  
+ 
 
 View(POP[["1990"]])
 

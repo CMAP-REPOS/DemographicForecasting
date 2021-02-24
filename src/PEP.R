b5649b788a7d1c8ad79169633064711c6cd37cbd
maryweber <67429420+maryweber@users.noreply.github.com>
2021-02-24 09:50:46 -0600

    pep changes

--- src/PEP.R
+++ src/PEP.R
@@ -23,7 +23,7 @@ CMAP_GEOIDS <- c("17031", "17043", "17089", "17093", "17097", "17111", "17197")
 #extra age groupings to remove
 remove <- c("Under 18 years", "5 to 13 years", "14 to 17 years", "18 to 64 years", "18 to 24 years", "25 to 44 years", 
             "45 to 64 years", "65 years and over", "85 years and over", "16 years and over", "18 years and over", 
-            "15 to 44 years", "Median age")
+            "15 to 44 years", "Median age", "All ages")
 
 # Compile population data from each Census. Always confirm that get_estimates() is pulling from latest vintage. 
 # Use ?get_estimates to see what the default vintage year is. If it's not the most recent year, update version of tidycensus (if a more recent version is available) ---------------
@@ -54,22 +54,21 @@ PEP_DATA <- PEP_DATA[-which(PEP_DATA$AGEGROUP %in% remove), ] #removes additiona
 
 for(YEAR in YEAR2) { 
   
-  POP[[as.character(YEAR)]] <- PEP_DATA %>%
-    mutate(AGEGROUP = case_when(AGEGROUP == "All ages" & Sex == 'Male' ~ "County Male Total",
-                                AGEGROUP == "All ages" & Sex == 'Female' ~ "County Female Total",
-                                TRUE ~ as.character(AGEGROUP)),
-           Region = case_when(GEOID %in% CMAP_GEOIDS ~ "CMAP Region",
+    POP[[as.character(YEAR)]] <- PEP_DATA %>%
+    rename(Population = value, Age = AGEGROUP) %>%
+    mutate(Region = case_when(GEOID %in% CMAP_GEOIDS ~ "CMAP Region",
                               State == "Illinois" ~ "External IL",
                               State == "Indiana" ~ "External IN",
-                              State == "Wisconsin" ~ "External WI")) 
+                              State == "Wisconsin" ~ "External WI"),
+           Age = str_replace_all(Age, "Age ", ""))
   
 }
 
 
 # Upload Excel files and save Output file (only necessary if changes to code/Excel data are made) -------------------------------
 
-POP[["1995"]] <- read_excel("Input/Pop1995.xlsx") 
-POP[["2005"]] <- read_excel("Input/Pop2005.xlsx") 
+#POP[["1995"]] <- read_excel("Input/Pop1995.xlsx") 
+#POP[["2005"]] <- read_excel("Input/Pop2005.xlsx") 
 
 #save(POP, file="Output/PopData.Rdata")
 #load("Output/PopData.Rdata")

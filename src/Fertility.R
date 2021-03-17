945d74d3f6966f057cb20f25620f095d83d508bf
maryweber <67429420+maryweber@users.noreply.github.com>
2021-03-17 11:23:42 -0500

    GQE

--- src/Fertility.R
+++ src/Fertility.R
@@ -13,11 +13,10 @@ load("Output/GQData.Rdata")
 
   GQE_YEARS <- c(2011:2019) #GQ estimate year range
   F_Groups <- c("15 to 19 years", "20 to 24 years", "25 to 29 years", "30 to 34 years", "35 to 39 years", "40 to 44 years")
-
   
 # Remove estimates of females in GQ from each year ----------------------------
 
-  # 2010 GQ population by county
+  # Step 1: 2010 GQ population by county
   GQ_totals_2010 <- GQ %>% 
     filter(Category == 'County Total')  %>% 
     filter(Concept != 'GROUP QUARTERS POPULATION IN MILITARY QUARTERS BY SEX BY AGE') %>% 
@@ -26,33 +25,28 @@ load("Output/GQData.Rdata")
     ungroup() %>%
     select(GEOID, County, County_GQtotal)
   
-  
-  # 2010 female GQ population by county, age
+  # Step 2: 2010 female GQ population by county, age
     FGQ_2010 <- GQ %>% 
       filter(Sex == 'Female')  %>% 
       filter(Concept != 'GROUP QUARTERS POPULATION IN MILITARY QUARTERS BY SEX BY AGE') %>% 
       filter(Age %in% F_Groups) %>%
       group_by(GEOID, County, State, Year, Region, Age, Sex) %>%
       summarise(Female_GQ = sum(Value)) 
-
     
-  # Calculate proportion of each GQ age group that is female
+  # Step 3: For each GQ female age group of interest, calculate what proportion of each county's total 2010 GQ population they represent
     GQ2010 <- left_join(FGQ_2010, GQ_totals_2010) %>% mutate(Prop_Female = Female_GQ/County_GQtotal)
   
- 
-  # GQ estimates 2011-2019 - by County only, not age/sex
+  # Step 4: Read in GQ Census estimates by State, County for 2011-2019
     GQE <- read_excel("Input/GQE.xlsx") 
     GQE$GEOID <- as.character(GQE$GEOID)
   
-  
-  # Multiply 2010 proportions by 2011-2019 GQ estimates to get expected number of females
+  # Step 5: Multiply 2010 proportions by 2011-2019 GQ estimates to get expected number of females
     GQE_projec <- left_join(GQ2010, select(GQE, c(GEOID, Year, Population)), by='GEOID') %>%
       mutate(GQE_pred = round(Prop_Female*Population),0) %>%
       rename(Year = Year.y) %>%
       select(GEOID, County, State, Sex, Age, GQE_pred, Year,Region)
   
-  
-  # Female HH population estimates by County, Age and Sex for 2011-2019 
+  # Step 6: Female HH population estimates by County, Age and Sex for 2011-2019 
     TEMP_DATA <- tibble()
     for (YEAR in GQE_YEARS) {
       Pop_Est <- POP[[as.character(YEAR)]] %>%
@@ -63,22 +57,19 @@ load("Output/GQData.Rdata")
       TEMP_DATA <- bind_rows(TEMP_DATA, Pop_Est)
     }
   
-    
-  # For 2011-2019 subtract expected GQ population from county Total to get County HH populations
+  # Step 7: For 2011-2019 subtract expected GQ population from county Total to get County HH populations
     HH_Pop <- left_join(GQE_projec, select(TEMP_DATA, c(GEOID, Year, Age, County_Total)), by=c('GEOID', 'Age', 'Year')) %>%
-      mutate(HH_Total = County_Total - GQE_pred) %>%
-      select(GEOID, County, State, Sex, Year, Age, HH_Total, Year,Region)
+      mutate(Population = County_Total - GQE_pred) %>%
+      select(GEOID, County, State, Sex, Year, Age, Population, Year,Region)
 
-
-  # 2010 Census HH Population 
+  # Step 8: Filter 2010 Decencial Census HH Data for females 15-44
     Female_HH_2010 <- POP[["2010"]]  %>%
         filter(Sex == 'Female') %>%
         filter(Age %in% F_Groups)
  
-
-  # Add 2010 Census HH Population data to 2011-2019 HH Population Estimates 
-    
-    
+  # Step 9: Merge 2010 Census HH Population data and 2011-2019 HH Population Estimates to form a complete table of Household Population data
+    F_Data <- bind_rows(Female_HH_2010, HH_Pop)
+ 
 
 # Birth data - add pre-age-15 births to 15-19 group, add 45+ age births to 40-44 group ------------------
   
@@ -89,12 +80,9 @@ load("Output/GQData.Rdata")
 
 # ASFR Calculation - # of live births per 1,000 women ------------------
 
-  F_DATA <- F_DATA %>% group_by(State, Age, Year) %>% summarise(Population = sum(Population), .groups="drop")
-  
   Births <- Births %>% group_by(State, Age, Year) %>% summarise(Births = sum(Births), .groups="drop")
   
   ASFR <- F_DATA %>% inner_join(Births, by = c("State", "Age", "Year")) %>% mutate(ASFR = round((Births/Population)*1000, 0))
-
 
 # sample plots ---------
 

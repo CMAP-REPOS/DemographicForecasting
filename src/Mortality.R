d148a25250184974f094fe43b10f6286f0d66966
maryweber <mweber@cmap.illinois.gov>
2021-06-03 12:50:31 -0500

    mortality

--- src/Mortality.R
+++ src/Mortality.R
@@ -12,24 +12,31 @@ library(readxl)
 load("Output/PopData.Rdata")
 Mort_YEARS <- c(2014:2018)
 
+MORT_DATA <- tibble()
 
 for (YEAR in Mort_YEARS) {
   Mort_Pop <- POP[[as.character(YEAR)]]
+  MORT_DATA <- bind_rows(MORT_DATA, Mort_Pop)
 }
 
-Mort_Pop$GEOID <- as.double(Mort_Pop$GEOID)
+
+MORT_DATA$GEOID <- as.double(MORT_DATA$GEOID)
 Deaths <- read_excel("Input/CMAPMortality1990-2019.xlsx")
 Deaths <- rename(Deaths, Age=Category)
 Deaths <- rename(Deaths, DeathCount=Value)
 Deaths <- Deaths %>% filter(Year > 2013 & Year < 2019)
 
+#i need to keep in the 0-4 age group to calculate population counts for 0-1 and 1-4
 
-m <- left_join(Deaths, Mort_Pop) %>% #i need population in here
-      mutate(Population = case_when(Age == '0 to 1 years' ~ DeathCount*(1/5), #this should be population
-                                   Age == '1 to 4 years' ~ DeathCount*(4/5),
-                                    TRUE ~ DeathCount))
+m <- full_join(Deaths, select(MORT_DATA, c(GEOID, Year, Age, Population)), by=c('GEOID', 'Age', 'Year')) %>%
+      mutate(Population2 = case_when(Age == '0 to 1 years' ~ Population*(1/5),
+                                   Age == '1 to 4 years' ~ Population*(4/5),
+                                    TRUE ~ Population))
 
-
+m <- full_join(Deaths, MORT_DATA) %>%
+  mutate(Population2 = case_when(Age == '0 to 1 years' ~ Population*(1/5),
+                                 Age == '1 to 4 years' ~ Population*(4/5),
+                                 TRUE ~ Population))
 View(m)
 
 

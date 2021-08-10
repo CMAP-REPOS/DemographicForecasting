872af6c434e8bf8f8568160f90b8a56b4c4ff24a
amcadamsCMAP <85572380+amcadamsCMAP@users.noreply.github.com>
2021-08-10 09:37:26 -0500

    Fixing Births calculation - 1 year cohorts

--- src/MigrationProjections.R
+++ src/MigrationProjections.R
@@ -33,7 +33,9 @@ ASFR_MidPoint <- ASFR_projections %>% pivot_wider(names_from = "Year", values_fr
                                   'ASFR2037.5'=rowMeans(across('2035':'2040')),
                                   'ASFR2042.5'=rowMeans(across('2040':'2045')), #if want to add midpoints for 2050-60, add mutates here
                                   'ASFR2047.5'=rowMeans(across('2045':'2050'))) %>%
-                                   select(c(1:2) | ends_with(".5"))
+                          rename_with(.fn = ~paste0("ASFR",.), .cols=starts_with("2")) %>%
+                          ungroup()
+                                  #select(c(1:2) | ends_with(".5"))
 #add in special calculation to combine 0-1 and 1-4 (David ER)
 
 #Step 3: Pull in 2020 PEP data
@@ -63,6 +65,23 @@ NetMig <- NetMig %>% filter(Age == 'Total' & Sex == 'Both') %>% select(-Period) 
 #multiply 2020 PEP by 2022.5 ASFRs to get expected births by age group, 2020-2025
 #sum expected births and multiply by average fertility ratio (not sure how this is calculated)
 F_Groups <- c("15 to 19 years", "20 to 24 years", "25 to 29 years", "30 to 34 years", "35 to 39 years", "40 to 44 years")
+
+projyears <- c("2020","2021","2022","2023","2024")
+
+projectedBirths <- PEP2020 %>%
+  filter(Sex == "Female", Age %in% F_Groups) %>%
+  full_join(select(ASFR_MidPoint, c(1:2,contains(all_of(projyears)))), by = c("Age", "Region")) %>%
+  mutate(Births2020 = Pop2020 * ASFR2020,
+         Births2021 = Pop2020 * ASFR2021,
+         Births2022 = Pop2020 * ASFR2022,
+         Births2023 = Pop2020 * ASFR2023,
+         Births2024 = Pop2020 * ASFR2024) %>%
+  select(c(1:2, starts_with("Births"))) %>%
+  group_by(Region)
+
+
+temp <- projectedBirths %>%
+  mutate(births = Pop2020 * "2020")
 
 projectedBirths <- PEP2020 %>%
   filter(Sex == "Female", Age %in% F_Groups) %>%

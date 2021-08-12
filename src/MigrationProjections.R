28d446515acaf22bc05396cb58465080fcb053b4
maryweber <mweber@cmap.illinois.gov>
2021-08-12 09:54:53 -0500

    reordered ages

--- src/MigrationProjections.R
+++ src/MigrationProjections.R
@@ -219,42 +219,28 @@ K_factors <- bind_rows(K_Under55, K_Over55) %>%
 
 # Step 7: Apply K factors to NMRs in order to calculate Net Migration
 
-
-
-
 Migration <- Base_Mig %>% select(Region, Age, Sex, NetRates) %>%
   pivot_wider(names_from = "Sex", values_from=c("NetRates")) %>%
   rename(Female_NMRs = Female, Male_NMRs = Male) %>%
   full_join(K_factors, by='Region') %>%
+  arrange(Region) %>%
   mutate(Female_Under.55 = case_when(Age %in% over55 ~ Female_Over.55,
                                      TRUE ~ Female_Under.55)) %>% select(-Female_Over.55) %>% rename(Female_K = Female_Under.55) %>%
   mutate(Male_Under.55 = case_when(Age %in% over55 ~ Male_Over.55,
                                      TRUE ~ Male_Under.55)) %>% select(-Male_Over.55) %>% rename(Male_K = Male_Under.55) %>%
-
-
   mutate(Male_NMR = case_when(Age %in% c('0 to 4 years', '5 to 9 years', '10 to 14 years') ~ (Male_NMRs + Female_NMRs + Female_K + Male_K)/2,
                               TRUE ~ Male_NMRs + Male_K)) %>%
-
   mutate(Male_NMR = case_when(Age == "50 to 54 years" ~ (lead(Male_NMR) + lag(Male_NMR))/2, #why is this not calculating as negative??
-                              TRUE ~ Male_NMR))
-
-
-
-
-
-
-
-K_factors <- K_factors %>% pivot_wider(names_from = "Sex", values_from=c("Under.55", "Over.55")) %>%
-  left_join(K_factors, by = c("Region", "Age")) %>%
-
-
-
-
+                              TRUE ~ Male_NMR)) %>%
+  mutate(Female_NMR = case_when(Age %in% c('0 to 4 years', '5 to 9 years', '10 to 14 years') ~ (Male_NMRs + Female_NMRs + Female_K + Male_K)/2,
+                            TRUE ~ Female_NMRs + Female_K)) %>%
+  mutate(Female_NMR = case_when(Age == "50 to 54 years" ~ (lead(Female_NMR) + lag(Female_NMR))/2,
+                              TRUE ~ Female_NMR))
 
 
 # Step 8: Apply Net Migration to Expected Population in order to calculate Projected Population
 
-
+Projections <- Migration
 
 
 # Step 9: Assemble Components of Change to check work (Optional)

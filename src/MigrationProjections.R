ed111150207a00851c473f7ffa7550db143e9c09
amcadamsCMAP <85572380+amcadamsCMAP@users.noreply.github.com>
2021-08-13 15:51:18 -0500

    Update of applying K factors to NMRs

--- src/MigrationProjections.R
+++ src/MigrationProjections.R
@@ -201,7 +201,7 @@ K_factors <- bind_rows(K_Under55, K_Over55) %>%
 
 # Step 6: Apply K factors to NMRs in order to calculate Net Migration
 
-Migration <- Base_Mig %>% select(Region, Age, Sex, NetRates) %>%
+Migration <- Base_Mig %>%
   left_join(K_factors, by=c('Region', 'Sex')) %>%
   pivot_wider(names_from = "Sex", values_from=c("NetRates", "Under.55", "Over.55")) %>%
   rename(K_U55_Female = Under.55_Female, K_O55_Female = Over.55_Female, K_U55_Male = Under.55_Male, K_O55_Male = Over.55_Male) %>%
@@ -211,26 +211,24 @@ Migration <- Base_Mig %>% select(Region, Age, Sex, NetRates) %>%
                               Age %in% c('15 to 19 years', '20 to 24 years', '25 to 29 years', '30 to 34 years', '35 to 39 years', '40 to 44 years', '45 to 49 years', '50 to 54 years') ~ NetRates_Male + K_U55_Male,
                               Age %in% c('55 to 59 years', '60 to 64 years', '65 to 69 years', '70 to 74 years', '75 to 79 years', '80 to 84 years', '85 years and over') ~ NetRates_Male + K_O55_Male)) %>%
 
-  mutate(Male_NMR = case_when(Age == "50 to 54 years" ~ (lead(Male_NMR) + lag(Male_NMR))/2, #why is this not calculating as negative??
+  mutate(Male_NMR = case_when(Age == "50 to 54 years" ~ (lead(Male_NMR) + lag(Male_NMR))/2,
                               TRUE ~ Male_NMR)) %>%
 
   mutate(Female_NMR = case_when(Age %in% c('0 to 4 years', '5 to 9 years', '10 to 14 years') ~ (NetRates_Female + NetRates_Male + K_U55_Male + K_U55_Female)/2,
                                 Age %in% c('15 to 19 years', '20 to 24 years', '25 to 29 years', '30 to 34 years', '35 to 39 years', '40 to 44 years', '45 to 49 years', '50 to 54 years') ~ NetRates_Female + K_U55_Female,
                                 Age %in% c('55 to 59 years', '60 to 64 years', '65 to 69 years', '70 to 74 years', '75 to 79 years', '80 to 84 years', '85 years and over') ~ NetRates_Female + K_O55_Female)) %>%
 
-  mutate(Male_NMR = case_when(Age == "50 to 54 years" ~ (lead(Female_NMR) + lag(Female_NMR))/2, #why is this not calculating as negative??
-                              TRUE ~ Male_NMR))
+  mutate(Male_NMR = case_when(Age == "50 to 54 years" ~ (lead(Female_NMR) + lag(Female_NMR))/2,
+                              TRUE ~ Male_NMR)) %>%
+  select(1:2 | ends_with("NMR")) %>%
+  pivot_longer(cols = ends_with("NMR"), names_to = "Sex", values_to = "NMRs") %>%
+  mutate(Sex = case_when(Sex == "Male_NMR" ~ "Male",
+                         TRUE ~ "Female"))
 
 
-# Step 8: Apply Net Migration to Expected Population in order to calculate Projected Population
+# Step 7: Apply Net Migration to Expected Population in order to calculate Projected Population
 
-Projections <- Migration %>% pivot_longer(cols = ends_with("NMR"),
-                                names_to = "Sex",
-                                values_to = "NMRs") %>% select(Region, Age, Sex, NMRs)
-
-Projections$Sex <- substr(Projections$Sex,1,nchar(Projections$Sex)-4)
-
-Projections <- Projections %>% left_join(expectedpop25, by=c("Region", "Age", "Sex")) %>% select(-Mort2022.5) %>% relocate(c(Pop2020, Pop2025), .before=NMRs) %>%
+Projections <- Migration %>% left_join(expectedpop25, by=c("Region", "Age", "Sex")) %>% select(-Mort2022.5) %>% relocate(c(Pop2020, Pop2025), .before=NMRs) %>%
                       mutate(NMs_Living =  Pop2025 * NMRs)  %>%
                       mutate(NMs_Living_Abs =  abs(Pop2025 * NMRs))
 

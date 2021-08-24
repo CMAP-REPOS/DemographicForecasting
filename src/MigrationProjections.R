fb0e5e24005d62723fe7814b9771c73d5cc126bf
amcadamsCMAP <85572380+amcadamsCMAP@users.noreply.github.com>
2021-08-24 15:38:09 -0500

    Update MigrationProjections.R

--- src/MigrationProjections.R
+++ src/MigrationProjections.R
@@ -11,7 +11,7 @@ load("Output/Mort_Proj.Rdata")    #named Mort_Proj
 load("Output/ASFR.Rdata")         #named ASFR_projections
 load("Output/BirthRatios.Rdata")  #named bRatios
 #load("Output/targetNM.Rdata")     #named target_NM
-target_NM <- read_excel("C:/Users/amcadams/OneDrive - Chicago Metropolitan Agency for Planning/Desktop/NetMigration_CMAP_workersjobsbalance.xlsx") %>%
+target_NM <- read_excel("C:/Users/amcadams/OneDrive - Chicago Metropolitan Agency for Planning/Desktop/NetMigrationflataverages.xlsx") %>%
   mutate(Year = as.character(Year))
 
 #load in variables from projection_control

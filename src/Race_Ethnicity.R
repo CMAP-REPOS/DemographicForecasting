5bcd12b3af4425f72603ba39bce8be431f49924c
Mary Weber <mweber@cmap.illinois.gov>
2021-10-14 16:24:12 -0500

    Create Race_Ethnicity.R

---
+++ src/Race_Ethnicity.R
@@ -0,0 +1,79 @@
+
+library(tidyverse)
+library(tidycensus)
+library(readxl)
+
+# Set parameters ----------------------------------------------------------
+
+Decennial_YEARS <- c(2010) #ideally add 2020 when that data is available
+Other_YEARS <- c(`4`=2011, `5`=2012, `6`=2013, `7`=2014, `8`=2015, `9`=2016, `10`=2017, `11`=2018, `12`=2019) # add in 2020 estimates
+YEARS <- c(2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019)
+
+COUNTIES <- list(
+  IL = c(31, 43, 89, 93, 97, 111, 197,       # CMAP counties
+         7, 37, 63, 91, 99, 103, 141, 201),  # Non-CMAP Illinois counties
+  IN = c(89, 91, 127),                       # Indiana counties
+  WI = c(59, 101, 127)                       # Wisconsin counties
+)
+
+CMAP_GEOIDS <- c("17031", "17043", "17089", "17093", "17097", "17111", "17197")
+IDs <- c('P005003', 'P005004', 'P005005', 'P005006', 'P005007','P005008', 'P005009','P005010')
+
+
+# Extract Decennial Census race/ethnicity variables from SF1 ---------------------------------
+
+Decennial_DATA <- tibble()
+POP <- list()
+for (YEAR in Decennial_YEARS) {
+  
+  # Compile list of variables to download
+  SF1_VARS <- load_variables(YEAR, "sf1")
+  POP_VARS <- SF1_VARS %>% filter(name %in% IDs) %>%
+    mutate(
+      Category = str_replace_all(label, "!!.*?", " "),
+      Category = str_replace(Category, ".*? ", "")
+    )
+  
+  for (STATE in names(COUNTIES)) {
+    TEMP <- get_decennial(geography = "county", variables = POP_VARS$name,
+                          county = COUNTIES[[STATE]], state = STATE,
+                          year = YEAR, survey = "sf1", cache_table = TRUE)%>%
+            separate(NAME, c("County", "State"), sep = "\\, ") %>%
+            rename(Population = value) %>%
+            mutate(variable = case_when(variable == 'P005003' ~ 'NH_White',
+                             variable == 'P005004' ~ 'NH_Black',
+                             variable == 'P005005' ~ 'NH_Other',
+                             variable == 'P005006' ~ 'NH_Asian',
+                             variable == 'P005007' ~ 'NH_Other',
+                             variable == 'P005008' ~ 'NH_Other',
+                             variable == 'P005009' ~ 'NH_Other',
+                             variable == 'P005010' ~ 'Hispanic'))
+ 
+      Decennial_DATA <- bind_rows(Decennial_DATA, TEMP)
+    }
+    
+    Decennial_DATA  <- Decennial_DATA %>% group_by(variable, County, State, GEOID) %>% 
+    summarise(Population = sum(Population)) #Berger lumped together all the 'other' groups into one
+    
+    Decennial_DATA$Year = '2010'
+}
+
+
+
+PEP_DATA <- tibble()
+
+for (STATE in names(COUNTIES)) {
+  PEP_TEMP <- get_estimates(product="characteristics", geography = "county", year = max(Other_YEARS),
+                            county = COUNTIES[[STATE]], state = STATE, breakdown = c("SEX", 'AGEGROUP', "RACE", "HISP"),
+                            breakdown_labels = TRUE, time_series=TRUE, show_call=TRUE) %>%
+    filter(DATE %in% names(Other_YEARS)) %>%
+    separate(NAME, c("County", "State"), sep = "\\, ") %>%
+    mutate(Year = Other_YEARS[as.character(DATE)],
+           Region = case_when(GEOID %in% CMAP_GEOIDS ~ "CMAP Region",
+                              State == "Illinois" ~ "External IL",
+                              State == "Indiana" ~ "External IN",
+                              State == "Wisconsin" ~ "External WI")) %>%
+          select(-DATE)
+  
+  PEP_DATA <- bind_rows(PEP_DATA, PEP_TEMP)
+}

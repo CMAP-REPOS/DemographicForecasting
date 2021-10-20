ca2c5243cabea4caa7b73372e5dd69c43d90a099
amcadamsCMAP <amcadams@cmap.illinois.gov>
2021-10-20 16:46:28 -0500

    add income and race/eth projection scripts

---
+++ src/income.R
@@ -0,0 +1,44 @@
+# This script pulls 5- year ACS (2015-2019) income data by household, calculates quartiles, and applies the rates forward
+# for all projection years (2025-2050).
+
+
+library(tidyverse)
+library(tidycensus)
+library(readxl)
+
+# Set parameters ----------------------------------------------------------
+
+load("Output/PumaRegions.Rdata") #"puma_region" - key for identifying CMAP region PUMAs. Created in Age_0_4_PUMS_Breakdown.R script
+
+
+
+# get data from ACS
+
+
+pums_il <- get_pums(variables = c("PUMA", "HINCP", "ADJINC"), state = "17", year = 2019, survey = "acs5",
+                    variables_filter = list(SPORDER = 1), rep_weights = "housing", show_call = TRUE)
+pums_in <- get_pums(variables = c("PUMA", "HINCP", "ADJINC"), state = "18", year = 2019, survey = "acs5",
+                    variables_filter = list(SPORDER = 1), rep_weights = "housing", show_call = TRUE)
+pums_wi <- get_pums(variables = c("PUMA", "HINCP", "ADJINC"), state = "55", year = 2019, survey = "acs5",
+                    variables_filter = list(SPORDER = 1), rep_weights = "housing", show_call = TRUE)
+
+pums_all <- bind_rows(pums_il, pums_in, pums_wi)
+
+
+pums_il_2 <- pums_il %>%
+  select(1:8) %>% left_join(puma_region, by=c("PUMA" = "PUMACE10", "ST" = "STATEFP10")) %>%
+  filter(SPORDER == "1") %>%
+  filter(!is.na(Region)) %>%
+  mutate(HHincome = HINCP * as.numeric(ADJINC)) %>%
+  group_by(Region) %>% summarize(totHH = sum(WGTP))
+
+
+cookco <- pums_il %>% filter(str_starts(PUMA, "031")) %>%
+  filter(HINCP >= 0) %>%
+  select(1:8)
+
+
+pums_in <- get_pums(variables = c("PUMA", "AGEP", "SEX"), state = "18", year = 2019, survey = "acs5",
+                    variables_filter = list(AGEP = 0:4), show_call = TRUE)
+pums_wi <- get_pums(variables = c("PUMA", "AGEP", "SEX"), state = "55", year = 2019, survey = "acs5",
+                    variables_filter = list(AGEP = 0:4), show_call = TRUE)
---
+++ src/race_ethnicity_projection.R
@@ -0,0 +1,3 @@
+# This script pulls in the projected Race/Ethnicity rates (projected in an Excel workbook)
+# and applies it to the projected population totals.
+

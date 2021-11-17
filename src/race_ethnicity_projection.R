5b5f08e459e69724be104dc7c397d1eefc86fed8
amcadamsCMAP <amcadams@cmap.illinois.gov>
2021-11-17 11:14:48 -0600

    cleaned up notes

--- src/race_ethnicity_projection.R
+++ src/race_ethnicity_projection.R
@@ -1,7 +1,8 @@
 #
 # This script pulls in the projected Race/Ethnicity rates (performed in an
 # Excel workbook and saved in Input/raceethrates.xlsx)
-# and applies it to the projected population totals.
+# and applies them to the projected population totals in order to produce
+# an estimate on the total projected population by race/ethnicity.
 #
 # Alexis McAdams, Mary Weber, November 2021
 
@@ -9,34 +10,36 @@ library(tidyverse)
 library(tidycensus)
 library(readxl)
 
+# Import population projection by age/sex
 load("Output/Migration_Projections.Rdata") #Mig_Proj
 
-#Import projected rates by race and ethnicity groupings (see Race_Ethnicity.R)
+#Import projected population rates by race and ethnicity groupings (from Race_Ethnicity.R)
 re_rates <- read_excel(path = "Input/raceethrates.xlsx") %>%
   pivot_longer(starts_with("2"), names_to = "Year", values_to = "Proportion")
 
-# join rates and projected population totals
+# Join R/E rates and projected population totals
 pop_summary <- Mig_Proj %>% group_by(year, Region) %>% summarize(TotPopulation = sum(ProjectedPop_final))
 
-# calculate total population by race and ethnicity group
+# Calculate total population by race and ethnicity grouping
 raceeth_proj <- full_join(re_rates, pop_summary, by=c("Year" = "year", "Region")) %>%
   filter(Year >=2025) %>%
   rowwise() %>%
   mutate(calcPop = round(TotPopulation * Proportion, 0)) %>%
   select(-Proportion, -TotPopulation)
 
-#export results
+# Save results (used in HH_Control.R)
 save(raceeth_proj, file = "Output/totalPop_RE_projection.R")
 
-#create alternate summary version
+# Create alternate summary version
 raceeth_proj_summary <- raceeth_proj %>%
   pivot_wider(names_from = Year, values_from = calcPop)
 
-#write.csv(raceeth_proj_summary, file = "C:/Users/amcadams/Documents/R/race_eth_projections.csv")
-
+# write summary version to disk
+#write.csv(raceeth_proj, file = "C:/Users/amcadams/Documents/R/race_eth_projections.csv")
 #write.csv(raceeth_proj_summary, file = "/Users/mweber/Desktop/raceeth_proj.csv")
 
-#check that population totals match (compare to pop_summary)
+
+# double-check that population totals match (compare to pop_summary abve)
 pop_summary2 <- raceeth_proj_summary <- full_join(re_rates, pop_summary, by=c("Year" = "year", "Region")) %>%
   filter(Year >=2025) %>%
   rowwise() %>%

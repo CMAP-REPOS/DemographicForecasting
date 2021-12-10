ec2b9f5eefca4a80ae103890a548edb4c57aa52d
amcadamsCMAP <amcadams@cmap.illinois.gov>
2021-12-10 13:25:37 -0600

    updated POP_PEP pull, added 2010+2015 extIL adj, reupload totalPopRE

--- src/race_ethnicity_projection.R
+++ src/race_ethnicity_projection.R
@@ -18,7 +18,10 @@ re_rates <- read_excel(path = "Input/raceethrates.xlsx") %>%
   pivot_longer(starts_with("2"), names_to = "Year", values_to = "Proportion")
 
 #combine known population and projected population totals
-load("Output/PopData.Rdata")  # named POP
+load("Output/POP_PEP.Rdata")  # named POP
+POP[["2010"]] <- read.csv("Input/adjustedCensus2010_ExtIL.csv") %>% mutate(GEOID = as.character(GEOID)) # ADJUSTMENT for Ext IL!
+POP[["2015"]] <- read.csv("Input/adjustedPEP2015_ExtIL.csv") %>% mutate(GEOID = as.character(GEOID))
+
 knownpop <- bind_rows(POP[['2015']], POP[['2020']] %>% mutate(GEOID = as.character(GEOID))) %>%
   group_by(Year, Region) %>% summarize(TotPopulation = sum(Population)) %>% ungroup() %>%
   mutate(year = as.character(Year)) %>% select(-Year)
@@ -44,7 +47,7 @@ raceeth_proj_summary <- raceeth_proj %>%
   pivot_wider(names_from = Year, values_from = calcPop)
 
 # write summary version to disk
-#write.csv(raceeth_proj, file = "C:/Users/amcadams/Documents/R/race_eth_projections.csv")
+write.csv(raceeth_proj, file = "C:/Users/amcadams/Documents/R/extILadj/race_eth_projections.csv")
 #write.csv(raceeth_proj_summary, file = "/Users/mweber/Desktop/raceeth_proj.csv")
 
 

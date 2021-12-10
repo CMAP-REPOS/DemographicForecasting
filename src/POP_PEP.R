6b1b3aa260fadafef5e375a9032c507bd9c6858f
amcadamsCMAP <85572380+amcadamsCMAP@users.noreply.github.com>
2021-12-10 09:22:17 -0600

    upload of Ext IL adj 2020 and updated POP

--- src/POP_PEP.R
+++ src/POP_PEP.R
@@ -115,12 +115,13 @@ for(YEAR in PEP_YEARS) {
 
 POP[["1995"]] <- read_excel("Input/Pop1995.xlsx")
 POP[["2005"]] <- read_excel("Input/Pop2005.xlsx")
-POP[["2020"]] <- read_excel("Input/censusadjustedPEP2020.xlsx")
+#POP[["2020"]] <- read_excel("Input/censusadjustedPEP2020.xlsx")
+POP[["2020"]] <- read_excel("Input/censusadjustedPEP2020_ExtILadj.xlsx")
 
 POP <- POP[as.character(sort(as.numeric(names(POP))))]
 
 
-#save(POP, file="Output/POP_PEP.Rdata")
+save(POP, file="Output/POP_PEP.Rdata")
 
 #write.csv(POP, "/Users/mweber/Desktop/POP_New.csv")
 

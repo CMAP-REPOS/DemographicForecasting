094a9311b4acd8da077cf94d9325ef434c143cf9
Haoyu Shi <hshi@cmap.illinois.gov>
2021-12-21 09:00:41 -0600

    Update Householders_by_Race_Ethnicity.R

--- src/Householders_by_Race_Ethnicity.R
+++ src/Householders_by_Race_Ethnicity.R
@@ -1,11 +1,14 @@
-# 12/16/2021
+# 12/16/2021 Haoyu Shi
 # Household Head Rate by Race and Ethnicity
 
 library(tidyverse)
+library(tidycensus)
+
+# I didn't use PEP data since there is no race/ethnicity information for housing units
 
 # Years
 Decennial_YEARS <- c(2010) #ideally add 2020 when that data is available
-Other_YEARS <- c(`4`=2011, `5`=2012, `6`=2013, `7`=2014, `8`=2015, `9`=2016, `10`=2017, `11`=2018, `12`=2019) # add in 2020 estimates
+Other_YEARS <- c(`4`=2011, `5`=2012, `6`=2013, `7`=2014, `8`=2015, `9`=2016, `10`=2017, `11`=2018, `12`=2019, `13` = 2020) # add in 2020 estimates
 YEARS <- c(2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019)
 
 # Geography
@@ -18,48 +21,128 @@ COUNTIES <- list(
 
 CMAP_GEOIDS <- c("17031", "17043", "17089", "17093", "17097", "17111", "17197")
 
+
+##### Extract Decennial Data
 # Variables
-IDs <- c('P005003', 'P005004', 'P005005', 'P005006', 'P005007','P005008', 'P005009','P005010') # population by race/ethnicity
-HH_Race <- c('H007003', 'H007004', 'H007005', 'H007006', 'H007007', 'H007008', 'H007009','H007010') # householder by race/ethnicity
+Decennial_ID <- c('P005003', #NH_White
+                      'P005004', #NH_Black
+                      'P005005', #NH_AmIn
+                      'P005006', #NH_Asian
+                      'P005007', #NH_PI              These are population
+                      'P005008', #NH_Other
+                      'P005009', #NH_More
+                      'P005010', #Hisp
 
-#####
-Decennial_DATA <- tibble()
-# POP <- list()
-
-for (STATE in names(COUNTIES)) {
-  TEMP <- get_decennial(geography = "county", variables = HH_Race,
-                        county = COUNTIES[[STATE]], state = STATE,
-                        year = 2010, survey = "sf1", cache_table = TRUE)%>%
-    separate(NAME, c("County", "State"), sep = "\\, ") %>%
-    rename(Householder = value) %>%
-    mutate(variable = case_when(variable == 'H007003' ~ 'NH_White',
-                                variable == 'H007004' ~ 'NH_Black',
-                                variable == 'H007005' ~ 'NH_Other',
-                                variable == 'H007006' ~ 'NH_Asian',
-                                variable == 'H007007' ~ 'NH_Other',
-                                variable == 'H007008' ~ 'NH_Other',
-                                variable == 'H007009' ~ 'NH_Other',
-                                variable == 'H007010' ~ 'Hispanic'))
-}
+                      'H007003', #NH_White
+                     'H007004', #NH_Black
+                     'H007005', #NH_AmIn
+                     'H007006', #NH_Asian
+                     'H007007', #NH_PI               These are householders
+                     'H007008', #NH_Other
+                     'H007009', #NH_More
+                     'H007010') #Hisp
 
 
-
-
+Decennial_Data <- tibble()
 
 for (YEAR in Decennial_YEARS) {
-
-  # Compile list of variables to download
-  SF1_VARS <- load_variables(YEAR, "sf1")
-  POP_VARS <- SF1_VARS %>% filter(name %in% IDs) %>%
-    mutate(
-      Category = str_replace_all(label, "!!.*?", " "),
-      Category = str_replace(Category, ".*? ", "")
-    )
-
   for (STATE in names(COUNTIES)) {
-    TEMP <- get_decennial(geography = "county", variables = IDs,
-                          county = COUNTIES[[STATE]], state = STATE,
-                          year = YEAR, survey = "sf1", cache_table = TRUE)%>%
+  TEMP <- get_decennial(geography = "county", variables = Decennial_ID,
+                                county = COUNTIES[[STATE]], state = STATE,
+                                year = YEAR, survey = "sf1", cache_table = TRUE)
+
+   Decennial_Data <- bind_rows(Decennial_Data, TEMP)
+  }
+}
+
+Decennial_Data <- Decennial_Data %>% spread(key = variable,
+                                            value = value)
+
+
+
+
+
+
+
+Decennial_Data$person_per_HH <- Decennial_Data$Population/Decennial_Data$Householder
+
+
+
+%>%
+  mutate(variable = case_when(variable == 'H007003' ~ 'NH_White',
+                              variable == 'H007004' ~ 'NH_Black',
+                              variable == 'H007005' ~ 'NH_Other',
+                              variable == 'H007006' ~ 'NH_Asian',
+                              variable == 'H007007' ~ 'NH_Other',
+                              variable == 'H007008' ~ 'NH_Other',
+                              variable == 'H007009' ~ 'NH_Other',
+                              variable == 'H007010' ~ 'Hispanic'))
+
+%>%
+  separate(NAME, c("County", "State"), sep = "\\, ") %>%
+  rename(Householder = value)
+
+
+
+
+%>%
+  mutate(variable = case_when(variable == 'P005003' ~ 'NH_White',
+                              variable == 'P005004' ~ 'NH_Black',
+                              variable == 'P005005' ~ 'NH_Other',
+                              variable == 'P005006' ~ 'NH_Asian',
+                              variable == 'P005007' ~ 'NH_Other',
+                              variable == 'P005008' ~ 'NH_Other',
+                              variable == 'P005009' ~ 'NH_Other',
+                              variable == 'P005010' ~ 'Hispanic'))
+
+%>%
+  separate(NAME, c("County", "State"), sep = "\\, ") %>%
+  rename(Population = value)
+
+##### Extract ACS Data (need to find more data for the ethnicity of householder. ACS only has Race information for householders.
+# only has NH-White householders and Hispanic/Latino householders )
+# Check the variables for ACS 5-year estimates
+ACS_VARS <- load_variables(year = 2019, dataset = "acs5")
+# get unique concepts
+unique_ACS_Concepts <- ACS_VARS$concept
+unique_ACS_Concepts <- as.data.frame(unique(unique_ACS_Concepts))
+
+ACS_POP_ID <- c("B03002_012", #total Hispanic/Latino
+                  "B03002_002", #total NH
+                  "B03002_003", #NH_White
+                  "B03002_004", #NH_Black
+                  "B03002_006") #NH_Asian
+
+ACS_HH_ID <- c("B22005I_001", #HISP
+               "B22005H_001", #NH_White
+
+
+)
+
+
+ACS_HH <- tibble()
+ACS_POP <- tibble()
+
+for (YEAR in Other_YEARS) {
+  for (STATE in names(COUNTIES)) {
+    HH_TEMP <- get_acs(geography = "county", variables = HH_Race,
+                             county = COUNTIES[[STATE]], state = STATE,
+                             year = YEAR, survey = "sf1", cache_table = TRUE)%>%
+      separate(NAME, c("County", "State","Year"), sep = "\\, ") %>%
+      rename(Householder = value) %>%
+      mutate(variable = case_when(variable == 'H007003' ~ 'NH_White',
+                                  variable == 'H007004' ~ 'NH_Black',
+                                  variable == 'H007005' ~ 'NH_Other',
+                                  variable == 'H007006' ~ 'NH_Asian',
+                                  variable == 'H007007' ~ 'NH_Other',
+                                  variable == 'H007008' ~ 'NH_Other',
+                                  variable == 'H007009' ~ 'NH_Other',
+                                  variable == 'H007010' ~ 'Hispanic'))
+
+
+    POP_TEMP <- get_acs(geography = "county", variables = POP_Race,
+                              county = COUNTIES[[STATE]], state = STATE,
+                              year = YEAR, survey = "sf1", cache_table = TRUE)%>%
       separate(NAME, c("County", "State"), sep = "\\, ") %>%
       rename(Population = value) %>%
       mutate(variable = case_when(variable == 'P005003' ~ 'NH_White',
@@ -71,27 +154,7 @@ for (YEAR in Decennial_YEARS) {
                                   variable == 'P005009' ~ 'NH_Other',
                                   variable == 'P005010' ~ 'Hispanic'))
 
-    Decennial_DATA <- bind_rows(Decennial_DATA, TEMP)
+    Decennial_HH <- bind_rows(Decennial_HH, HH_TEMP)
+    Decennial_POP <- bind_rows(Decennial_POP, POP_TEMP)
   }
-
-  Decennial_DATA  <- Decennial_DATA %>% group_by(variable, County, State, GEOID) %>%
-    summarise(Population = sum(Population)) #Berger lumped together all the 'other' groups into one
-
-  Decennial_DATA$Year = '2010'
-
-  Decennial_DATA <- Decennial_DATA %>%
-    mutate(Region = case_when(GEOID %in% CMAP_GEOIDS ~ "CMAP Region",
-                              State == "Illinois" ~ "External IL",
-                              State == "Indiana" ~ "External IN",
-                              TRUE ~ "External WI"))
 }
-
-DecennialSummary <- Decennial_DATA %>% group_by(Region, Year, variable) %>% summarize(Population = sum(Population)) %>%
-  mutate(RACE = case_when(str_detect(variable, "Asian") ~ "Asian alone",
-                          str_detect(variable, "Black") ~ "Black alone",
-                          str_detect(variable, "White") ~ "White alone",
-                          str_detect(variable, "Other") ~ "NH_Other",
-                          TRUE ~ "All races" ) ) %>%
-  mutate(HISP = case_when(str_detect(variable, "Hispanic") ~ "Hispanic",
-                          TRUE ~ "Non-Hispanic")) %>%
-  select(-variable) %>% pivot_wider(names_from = Year, values_from = Population)

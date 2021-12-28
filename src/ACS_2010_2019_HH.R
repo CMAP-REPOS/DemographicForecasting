54d44920b010484e41ef1568bcd0ee2e25973f8a
Haoyu Shi <hshi@cmap.illinois.gov>
2021-12-28 09:29:03 -0600

    Update ACS_2010_2019_HH.R

--- src/ACS_2010_2019_HH.R
+++ src/ACS_2010_2019_HH.R
@@ -4,6 +4,7 @@
 
 library(tidycensus)
 library(tidyverse)
+library(tidyr)
 
 # Geography
 COUNTIES <- list(
@@ -19,38 +20,65 @@ CMAP_GEOIDS <- c("17031", "17043", "17089", "17093", "17097", "17111", "17197")
 Years <- c(2010:2019)
 
 ######################### Variables ID ###############################
-POP_ID <- c("B02001_001", # Population Total
-            "B02001_002", # White
-            "B02001_003", # Black
-            "B02001_005", # Asian
-            "B03002_003", # NH_White
-            "B03002_012") # Hispanic
+POP_ID <- c(POP_Total = "B02001_001", # Population Total
+            POP_White = "B02001_002", # White
+            POP_Black = "B02001_003", # Black
+            POP_Asian = "B02001_005", # Asian
+            POP_NH_White = "B03002_003", # NH_White
+            POP_HISP = "B03002_012") # Hispanic
 
 
-HH_ID <- c("B25006_001", # Householder Total
-           "B25006_002", # White
-           "B25006_003", # Black
-           "B25006_005", # Asian
-           "B22005H_001", # NH_White
-           "B22005I_001") # Hispanic
+HH_ID <- c(HH_Total = "B25006_001", # Householder Total
+           HH_White = "B25006_002", # White
+           HH_Black = "B25006_003", # Black
+           HH_Asian = "B25006_005", # Asian
+           HH_NH_White = "B22005H_001", # NH_White
+           HH_HISP = "B22005I_001") # Hispanic
 
 ################ Retrieving data using tidycensus ##################
-ACS_POP <- tibble()
-ACS_HH <- tibble()
+ACS_Data <- tibble()
 
 for (YEAR in Years){
   for (STATE in names(COUNTIES)){
-    TEMP <- get_acs(geography = "county", variables = POP_ID,
+    TEMP <- get_acs(geography = "county", variables = c(POP_ID,HH_ID),
                           county = COUNTIES[[STATE]], state = STATE,
-                          year = YEAR, survey = "acs5", cache_table = TRUE) %>%
-    separate(NAME, c("County", "State"), sep = "\\, ") %>%
-    mutate(variable = case_when(variable == "B02001_001" ~ "All",
-                                variable == "B02001_002" ~ "White",
-                                variable == "B02001_003" ~ "Black",
-                                variable == "B02001_005" ~ "Asian",
-                                variable == "B03002_003" ~ "NH_White",
-                                variable == "B03002_012" ~ "Hispanic"))
+                          year = YEAR, survey = "acs5", cache_table = TRUE, output = "wide") %>%
+      select(-ends_with("M")) %>%
+      separate(NAME, c("County", "State"), sep = "\\, ")
+
     TEMP$Year = YEAR
-    ACS_POP <- bind_rows(ACS_POP, TEMP)
+    ACS_Data <- bind_rows(ACS_Data, TEMP)
   }
 }
+##### Calculating Data
+ACS_Data_Processed <- ACS_Data %>%
+  mutate(POP_Other = POP_TotalE - POP_WhiteE - POP_BlackE - POP_AsianE,
+         POP_NH = POP_TotalE -POP_HISPE,
+         HH_Other = HH_TotalE - HH_WhiteE - HH_BlackE - HH_AsianE,
+         HH_NH = HH_TotalE - HH_HISPE)
+
+##### Making it into a longer table
+ACS_Pop <- ACS_Data_Processed %>% select("GEOID","County", "State", "Year", starts_with("POP"))
+names(ACS_Pop) <- c("GEOID","County", "State", "Year","All", "White", "Black", "Asian", "NH_White", "Hispanic", "Other", "NH")
+
+ACS_HH <- ACS_Data_Processed %>% select("GEOID", "County", "State", "Year", starts_with("HH"))
+names(ACS_HH) <- c("GEOID","County", "State", "Year","All", "White", "Black", "Asian", "NH_White", "Hispanic", "Other", "NH")
+
+POP_longer <- ACS_Pop %>%
+  pivot_longer(
+    cols = c("All", "White", "Black", "Asian", "NH_White", "Hispanic", "Other", "NH"),
+    names_to = "Race",
+    values_to = "Population"
+  )
+
+HH_longer <- ACS_HH %>%
+  pivot_longer(
+    cols = c("All", "White", "Black", "Asian", "NH_White", "Hispanic", "Other", "NH"),
+    names_to = "Race",
+    values_to = "Householder"
+  )
+
+ACS_longer <- right_join(POP_longer, HH_longer)
+ACS_longer$head.rate <- ACS_longer$Householder/ACS_longer$Population
+
+write.csv(ACS_longer, "C:/Users/hshi/Desktop/Demographic_Prediction/ACS-5_head_rate_2010_to_2019.csv")

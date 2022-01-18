f8f88f39e9e227afa165999e49dc5e9bfd784fa8
amcadamsCMAP <85572380+amcadamsCMAP@users.noreply.github.com>
2022-01-18 17:43:12 -0600

    add'l notes, small changes to make script more dynamic

--- src/Mortality.R
+++ src/Mortality.R
@@ -10,9 +10,9 @@ library(readxl)
 load("Output/POP_PEP.Rdata")
 #load("Output/PopData.Rdata") #OLD, do not use!
 load("Output/Age_0_4_Freq.Rdata") #AGE_0_4_FREQ
-AGE_0_4_FREQ$Population <- as.numeric(AGE_0_4_FREQ$Population)
 
 MORT_YEARS <- c(2014:2018)
+
 DEATHS_XLSX <- "Input/CMAPMortality1990-2019.xlsx"
 
 # Create mortality age groups ---------------------------------------------------------
@@ -46,10 +46,7 @@ MORT_DATA <- MORT_POP %>%
 AGE_0_4_PROP <- AGE_0_4_FREQ %>% mutate(Age = case_when(AgeGroup == 'Less than 1 year' ~ '0 to 1 years',
                                         TRUE ~ AgeGroup)) %>% select(-AgeGroup, -Population)
 
-
 MORT_DATA <- MORT_DATA %>% left_join(AGE_0_4_PROP, by=c('Age','Region', 'Sex'))
-
-
 
 MORT_DATA <- MORT_DATA %>% mutate(Population = case_when(Age == '0 to 1 years' ~ lead(Population)*Age_0_4_Share,
                                                          Age == '1 to 4 years' ~ lag(Population)*Age_0_4_Share,
@@ -94,21 +91,22 @@ LifeTable <- MORT_DATA %>%
 
 # Read in SSA tables -----------------------------------------------------------
 
-SSA <- read_excel("Input/SSA.xlsx")
+SSA <- read_excel("Input/SSA.xlsx") #Source: Table by DE-R (see Documentation), SSA forecast values from 2019 Annual Trustees Report
+
 
 # Create final projections for each region  ------------------------------------
 
 Mort_Proj <- LifeTable %>%
   select(Region, Sex, Age, Sx) %>%
   left_join(SSA, by= c("Sex", "Age")) %>%
-  mutate(across(c(5:13), .fns = ~.*Sx)) %>%
+  mutate(across(starts_with("2"), .fns = ~.*Sx)) %>%
   #select(-Sx)
   rename("2018" = Sx) #keep the calculated Sx
 
-# Clean-up to values >= 1  ------------------------------------ This could use some adjustments to make it more dynamic
+# Fix values > 1  ------------------------------------ This could use some adjustments to make it more dynamic
 
 temp <- Mort_Proj %>% filter_at(vars(4:13), any_vars(. >= 1))
-temp[7:13] <- temp$'2035'
+temp[13] <- temp$'2055'
 #view(temp)
 
 Mort_Proj <- rbind(Mort_Proj, temp)
@@ -124,10 +122,10 @@ Mort_Proj <- Mort_Proj %>% mutate('Mort2022.5'=rowMeans(across('2020':'2025')),
                                       'Mort2047.5'=rowMeans(across('2045':'2050')),
                                       'Mort2052.5'=rowMeans(across('2050':'2055')),
                                       'Mort2057.5'=rowMeans(across('2055':'2060'))) %>%
-                                      select(c(1:3) | starts_with("Mort"))
+                                      select(!starts_with("2") | starts_with("Mort"))
 
 
 
-#save(Mort_Proj, file="Output/Mort_Proj.Rdata")
+save(Mort_Proj, file="Output/Mort_Proj.Rdata")
 #save(LifeTable, file="Output/LifeTables.Rdata")
 

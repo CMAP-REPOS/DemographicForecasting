1999a9cb88b5e3d97c1d126436ee95100e639f17
amcadamsCMAP <amcadams@cmap.illinois.gov>
2022-01-19 12:16:38 -0600

    add placeholder for c19, edit f adj

--- src/setup_control.R
+++ src/setup_control.R
@@ -32,20 +32,27 @@ save(COUNTIES, CMAP_GEOIDS, file="Output/importhelpers.Rdata")
 ######### set options -----------------
 
 #### set External Illinois Adjustment area Base Population override
-    # this option changes the Base Year (2020) population.
+    # this option changes the Base Year (2020) population in the Projection_Control.R script.
     # this option does NOT recalculate fertility/mortality/migration rates (county or region-wide rates will be applied.)
 # if 1: model will use travel model boundary-adjusted population for LOL (Lee, Ogle, LaSalle) counties
 # if 0: model will use full county populations for the LOL counties, no adjustment will be applied.
 
-EXTIL = 1
+EXTIL = 0
 
 #### fertility method choice
-    # this option changes the method of the fertility calculation.
+    # this option changes the method of the fertility calculation in Projection.R.
 # if 1: model will use Approach A: log projection of 1990-2010 ASFRs, summed by Region
 # if 0: model will use Approach B: calculate recent (2014-2018) ASFRs, summed by Region,
 #       and project using trends outlined by Census Bureau's National Projections (2014)
 
 fMethod = 1
+
+#### placeholder - COVID-19 Base Population Adjustment
+    # this option will change the Base Year (2020) population .
+    # this option does NOT alter the mortality rates (reflecting assumption that post-COVID death rates will
+    # return to pre-COVID forecasted rates.)
+
+c19deaths = 0
 
 
 ########## run scripts to prepare set-up for projection -----------------
@@ -56,14 +63,17 @@ source("src/Mortality.R")
 source("src/GroupQuarters.R")
 
 if(fMethod = 1){
-
-
+  ASFR_projections <- read_excel("Input/berger_ASFRs.xlsx") %>%
+    filter(Age != "10 - 14") %>%
+    filter(Age != "45 - 49")
+  save(ASFR_projections, file="Output/ASFR.Rdata")
 
 }else if(fMethod = 0){
-
   source("src/Fertility.R")
 
 }else{
+
+  print("ERROR! Invalid ASFR method chosen, edit fMethod variable and run script again." )
 
 }
 

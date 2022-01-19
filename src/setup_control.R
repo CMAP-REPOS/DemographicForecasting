2aec235e494c9055eb4307bc25fcc470bb39b829
amcadamsCMAP <amcadams@cmap.illinois.gov>
2022-01-19 13:46:42 -0600

    move EXTIL and c19 adjustments to proj_control

--- src/setup_control.R
+++ src/setup_control.R
@@ -31,14 +31,6 @@ save(COUNTIES, CMAP_GEOIDS, file="Output/importhelpers.Rdata")
 
 ######### set options -----------------
 
-#### set External Illinois Adjustment area Base Population override
-    # this option changes the Base Year (2020) population in the Projection_Control.R script.
-    # this option does NOT recalculate fertility/mortality/migration rates (county or region-wide rates will be applied.)
-# if 1: model will use travel model boundary-adjusted population for LOL (Lee, Ogle, LaSalle) counties
-# if 0: model will use full county populations for the LOL counties, no adjustment will be applied.
-
-EXTIL = 0
-
 #### fertility method choice
     # this option changes the method of the fertility calculation in Projection.R.
 # if 1: model will use Approach A: log projection of 1990-2010 ASFRs, summed by Region
@@ -47,12 +39,6 @@ EXTIL = 0
 
 fMethod = 1
 
-#### placeholder - COVID-19 Base Population Adjustment
-    # this option will change the Base Year (2020) population .
-    # this option does NOT alter the mortality rates (reflecting assumption that post-COVID death rates will
-    # return to pre-COVID forecasted rates.)
-
-c19deaths = 0
 
 
 ########## run scripts to prepare set-up for projection -----------------

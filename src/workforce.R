d8434ac2a53f6885041c5788f3e5ae870332c7c6
amcadamsCMAP <85572380+amcadamsCMAP@users.noreply.github.com>
2022-01-26 08:50:22 -0600

    clean-up, extra notes

--- src/workforce.R
+++ src/workforce.R
@@ -1,12 +1,12 @@
 # Workforce.R
 #
-# This script estimates the number of workers from several inputs: total
+# This script estimates the number of workers in households from several inputs: total
 # population by age and sex (Census and CMAP Projection), estimates of Group
 # Quarter populations by age and sex (Census and CMAP Projection), national age- and sex-
 # specific Labor Force Participation Rates (Source: CBO's "2021 Long-Term Budget
 # Outlook", interpolated and held constant post-2030), national annual
 # non-Military 16+ Unemployment rates (source: CBO's "An Update to the Budget
-# and Economic Outlook: 2021 to 2031", held constant post-2030)
+# and Economic Outlook: 2021 to 2031", held constant post-2030 (~4.5%))
 #
 # Alexis McAdams, CMAP, Oct 2021
 
@@ -15,25 +15,39 @@ library(dplyr)
 library(tidyverse)
 library(readxl)
 
-#get GQ ratios from GroupQuarters.R
+##### Set-up
+# GQ ratios from GroupQuarters.R
 load("Output/GQData2.Rdata") # named GQratios
 
-#define "too young to work" age groups (assumption)
+# define "too young to work" age groups (assumption)
 kiddos <- c("0 to 4 years", "5 to 9 years", "10 to 14 years")
 
-#import Labor Force Participation Rate projections
+# import Labor Force Participation Rate projections (15 and up)
 LFPRs <- read.csv("Input/LFPRs.csv")
 
-#import unemployment rates (historical and projections)
+# import unemployment rates (historical and projections)
 unemp <- read.csv("Input/unemploymentrates.csv") %>%
   mutate(Year = as.character(Year))
 
-#re-load in and format known population data (2010-2020)
+#######re-load in and format known population data (2010-2020)
 # POP was originally retrieved in Projection.R
 load("Output/POP_PEP.Rdata")  # named POP
+
+# apply EXTIL adjustment (if necessary)
+if(EXTIL == 1){
+  POP[["2020"]] <- read_excel("Input/censusadjustedPEP2020_ExtILadj.xlsx") %>% # partial LOL counties
+    mutate(GEOID = as.character(GEOID))
+} else if (EXTIL == 0){
+  # no modification to POP file.
+  print("ExtIL Area Adjustment override NOT implemented.")
+}else {
+  print("ERROR! Improper EXTIL value supplied. Modify and run again.")
+}
+
+#create POPrecent list
 POPrecent <- list()
 POPrecent[["2010"]] <- POP[["2010"]]
-#POPrecent[["2015"]] <- POP[["2015"]]
+#POPrecent[["2015"]] <- POP[["2015"]] # TO-DO: build in option to look at 15,18,19 (for comparison to EMP employment forecast)
 #POPrecent[["2018"]] <- POP[["2018"]]
 #POPrecent[["2019"]] <- POP[["2019"]]
 POPrecent[["2020"]] <- POP[["2020"]]
@@ -68,7 +82,7 @@ for(i in names(laborforce)){
 
 #join the LFPRs to each of the tables in laborforce
 laborforce[["2010"]] <- laborforce[["2010"]] %>% left_join(LFPRs[c(1:2,3)], by=c("Age","Sex")) %>% rename(LFPR = LFPR2010)
-#laborforce[["2015"]] <- laborforce[["2015"]] %>% left_join(LFPRs[c(1:2,4)], by=c("Age","Sex")) %>% rename(LFPR = LFPR2015)
+#laborforce[["2015"]] <- laborforce[["2015"]] %>% left_join(LFPRs[c(1:2,4)], by=c("Age","Sex")) %>% rename(LFPR = LFPR2015) #see line 49 above
 #laborforce[["2018"]] <- laborforce[["2018"]] %>% left_join(LFPRs[c(1:2,5)], by=c("Age","Sex")) %>% rename(LFPR = LFPR2018)
 #laborforce[["2019"]] <- laborforce[["2019"]] %>% left_join(LFPRs[c(1:2,6)], by=c("Age","Sex")) %>% rename(LFPR = LFPR2019)
 laborforce[["2020"]] <- laborforce[["2020"]] %>% left_join(LFPRs[c(1:2,7)], by=c("Age","Sex")) %>% rename(LFPR = LFPR2020)
@@ -97,15 +111,15 @@ for(item in laborforce){
   i <- i + 1
 }
 
-#total up laborforce, join with unemployment rate, calculate number of workers
+#total up labor force, join with unemployment rate, calculate number of workers
 workers <- workers %>%
   #select(-starts_with("LFPR")) %>% #filter out to select an employment forecast
   group_by(Region, year) %>%
-  summarize(totlaborforce = sum(laborforce)) %>%
+  summarize(totlaborforce = sum(laborforce), .groups = "drop") %>%
   left_join(unemp, by=c("year" = "Year")) %>%
   mutate(workers = totlaborforce * (1 - (Unemployment.Rate / 100))) %>%
   select(-totlaborforce, -Unemployment.Rate) %>%
-  mutate(workers = round(workers * 1.00113,0)) %>% #constant to account for jobs held by people commuting from out of region (Berger's #)
+  mutate(workers = round(workers * 1.00113,0)) %>% # constant to account for jobs held by people commuting from out of region (Berger's #)
   mutate(Year = as.numeric(year)) %>% select(-year) %>%
   mutate(type = case_when(Year < 2021 ~ "PEPestimate",
                           TRUE ~ "Projection"))

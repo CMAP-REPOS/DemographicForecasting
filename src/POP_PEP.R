33314b9f935d23e0aafab80be70f74744c208833
AbahlsCMAP <abahls@cmap.illinois.gov>
2024-02-21 13:05:02 -0600

    clean up/finish documenting POP file
    still need to think about incorporating 2023 PEP data, currently not accessible via tidycensus get_estimates like other years

--- src/POP_PEP.R
+++ src/POP_PEP.R
@@ -1,11 +1,9 @@
 # CMAP | Noel Peterson, Mary Weber, Alexis McAdams, Alex Bahls | 7/11/2023
 
-#AB -- old script uses 1990, not sure if we need it/how many years we actually need
-#AB -- will want to add years as time goes on
-#AB -- get estimates doenst really work past 2019 for now, hopefully gets updated
+#AB -- get estimates isn't updated for 2023 -- hoping to wait for consistency but may need to manually download and incorp
 
 # This script fetches and formats 2000, 2010, and 2020 Decennial Census data AND
-# 2021 Population Estimates Program (PEP) data.
+# 2021, and 2022 Population Estimates Program (PEP) data.
 # Population data is pulled at the county level by age (5-year groupings) and sex.
 # Data for 1995, 2005 and 2020 is imported MANUALLY from prepared excel csvs (see end of script).
 # Data is combined into a single list object called POP.
@@ -16,18 +14,19 @@ library(readxl)
 
 # Set parameters ----------------------------------------------------------
 
+#COUNTY and GEOid list should be loaded from (full_run.R)
+
 ### Years for which to pull data
 #Census Population
 POP_YEARS <- c(2000, 2010, 2020)  # 1990 not available via API
 
-#Census Population Estimates Program (PEP)
+#Census Population Estimates Program (PEP) -- list of years to pull
 PEP_YEARS <- c(`2023`= 2023, `2021` = 2021, `2022` = 2022, `4`=2011, `5`=2012, `6`=2013, `7`=2014, `8`=2015, `9`=2016, `10`=2017, `11`=2018, `12`=2019) #2020 is based on 2010 vintage and doesnt use census
+
+#note that including 2023 doesnt result in an error but also the data isn't downloaded later on (yet)
 
 #  note: `#` names in PEP_YEARS are necessary to filter correct years of data from PEP pull (see tidycensus documentation, "time_series" argument)
 # PEP DATE_CODE documentation: https://www.census.gov/programs-surveys/popest/technical-documentation/research/evaluation-estimates/2020-evaluation-estimates/2010s-county-detail.html
-
-### import helpers
-# COUNTIES and CMAP_GEOIDS
 
 # Table names for Decennial Census data pull
 POP_TABLES <- c(
@@ -63,14 +62,15 @@ for (YEAR in POP_YEARS) {
     TEMP <- get_decennial(geography = "county", variables = POP_VARS$name,
                           county = COUNTIES[[STATE]], state = STATE,
                           year = YEAR, survey = "sf1", cache_table = TRUE)
-    POP_DATA <- bind_rows(POP_DATA, TEMP)
+    POP_DATA <- bind_rows(POP_DATA, TEMP) |> distinct()
   }
 
   POP[[as.character(YEAR)]] <- POP_DATA %>%
     left_join(POP_VARS, by = c("variable" = "name")) %>%
     select(-variable, -label, -concept) %>%
-    separate(NAME, c("County", "State"), sep = "\\, ") %>%
-    separate(Category, c("Sex", "Age"), sep = " ", extra = "merge") %>%
+    filter(!is.na(Category) & !(Category  %in% c("Total","Male","Female"))) |> #not necessary but stops warnings later
+    separate_wider_delim(NAME, delim = ",", names = c("County", "State")) |> #updated from depreciated command
+    separate_wider_delim(Category, delim = " ", names = c("Sex","Age"), too_many = "merge") |>
     rename(Population = value) %>%
     mutate(
       Year = YEAR,
@@ -177,8 +177,6 @@ for (STATE in names(COUNTIES)) {
 
 
 
-
-
 # Combine POP and PEP data in the POP list ----------------
 
 for(YEAR in PEP_YEARS) {
@@ -201,3 +199,5 @@ POP <- POP[as.character(sort(as.numeric(names(POP))))]
 
 # save POP list to Output folder
 save(POP, file="Output/POP_PEP.Rdata")
+
+rm(list=setdiff(ls(), c("POP", "COUNTIES", "CMAP_GEOIDS")))
